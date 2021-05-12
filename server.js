29c787e14ad4ee7c913f4740f87f4cad983cadf1
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-05-12 18:47:26 +0800

    enhanced server.js file. used express.static for routing HTML static files, added 404 route, modified api routing to load from notes.js file (still WIP)

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-// Dependencies
 const express = require('express');
 const path = require('path');
-
-// Tells node that we are creating an "express" server
-// // Sets up the Express App
+const notes = require('./routes/notes');
 const app = express();
-// Running on port 3050
-const PORT = 3050;
+const PORT = 5050;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Define routes
-// // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+app.use('/api/notes', notes);
+app.use(express.static('public', {
+    extensions: ['html']
+}));
 
+// 404 Route 
+app.use(function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-// Checking if server is created and listening
 app.listen(PORT, () => {
     console.log(`Note Taker app is currently listening on PORT: ${PORT}`);
 });
