ec47635d7a01a28d2765815b1165886cf61b5b0d
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-05-13 14:19:03 +0800

    added in fs.readFile to help catch potential errors that may arise in the get and post. Pseudo code written for router.delete, and added readFile to catch errors

--- routes/notes.js
+++ routes/notes.js
@@ -8,37 +8,63 @@ const { v4: uuidv4 } = require("uuid");
 module.exports = (router) => {
 	// * `GET /api/notes/` should read the `db.json` file and return all saved notes as JSON.
 	router.get("/", (req, res) => {
-		res.json(notesDB);
+		fs.readFile("../db/db.json", "utf8", (err, notesDB) => {
+			if (err) throw err;
+			let notes = JSON.parse(notesDB);
+			res.json(notes);
+		});
 	});
 
 	// * `POST /api/notes` should receive a new note to save on the request body,
 	// add it to the `db.json` file, and then return the new note to the client.
 	router.post("/", (req, res) => {
-		const notesID = uuidv4();
-		const notesData = {
-			title: req.body.title,
-			text: req.body.text,
-			id: notesID,
-		};
 
-		console.log(" ----- This is testing the notesData ----- ", notesData);
+		fs.readFile(__dirname + "../db/db.json", 'utf8', (error, notesDB) => {
+			if (error) throw err;
+			let notes = JSON.parse(notesDB);
+			let notesID = uuidv4();
 
-		notesDB.push(notesData);
+			const newNotesData = {
+				title: req.body.title,
+				text: req.body.text,
+				id: notesID,
+			};
 
-		fs.writeFile("../db/db.json", JSON.stringify(notesDB), (err) => {
-			if (err) throw (err);
-			console.log(" ----- Successfully saved the new note! ----- ");
+			console.log(" ----- This is testing the notesData ----- ", newNotesData);
+
+			let activeNote = notes.concat(newNotesData);
+
+			fs.writeFile(__dirname + "../db/db.json", JSON.stringify(activeNote), (error, activeNote) => {
+				if (error) {return error};
+				console.log("This is a console log for activeNote data ---> ", activeNote);
+				res.json(activeNote);
+			});
 		});
-
-		res.json(notesDB);
 	});
 
-	// * `DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete. 
-	// In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, 
+	// * `DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete.
+	// In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property,
 	// and then rewrite the notes to the `db.json` file.
 	router.delete("/:id", (req, res) => {
 
 		console.log(" ----- This is a delete test for some notes ----- ", notesDB);
 
+		let notesID = JSON.parse(req.params.id);
+
+		console.log("This is a console log for the notesID ---> ", notesID);
+
+		fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+			if (error) {
+			  return console.log(error)
+			}
+
+			// After read, locate file via notesID
+
+			// upon locating notesID, delete.
+		})
+		
+
 	});
+
+	// If have time, put an edit notes function (router.put)?
 };
